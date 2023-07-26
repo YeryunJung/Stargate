328e64b41d3dbf8d48f8dd23047e49877bd25622
hsilnam <hsilnam3@gmail.com>
2023-07-26 16:08:20 +0900

    refactor: 필요없는 파일, 코드 삭제

--- stargate-be/src/main/java/com/ssafy/stargate/model/service/DashboardService.java
+++ stargate-be/src/main/java/com/ssafy/stargate/model/service/DashboardService.java
@@ -1,10 +1,4 @@
 package com.ssafy.stargate.model.service;
 
-import com.ssafy.stargate.model.dto.common.MeetingDto;
-import com.ssafy.stargate.model.dto.response.MeetingListResponseDto;
-
-import java.security.Principal;
-import java.util.List;
-
 public interface DashboardService {
 }
--- stargate-be/src/main/java/com/ssafy/stargate/model/service/DashboardServiceImpl.java
+++ stargate-be/src/main/java/com/ssafy/stargate/model/service/DashboardServiceImpl.java
@@ -1,15 +1,9 @@
 package com.ssafy.stargate.model.service;
 
-import com.ssafy.stargate.model.dto.common.MeetingDto;
-import com.ssafy.stargate.model.dto.response.MeetingListResponseDto;
-import com.ssafy.stargate.model.entity.Meeting;
 import com.ssafy.stargate.model.repository.MeetingRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.security.Principal;
-import java.util.List;
 
 
 @Service
