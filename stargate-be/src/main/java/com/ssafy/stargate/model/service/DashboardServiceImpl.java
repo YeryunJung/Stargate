1f79c0329373b6f1099f87d08dace7aa9e99071b
dohyeon <beigemint26@gmail.com>
2023-08-03 14:21:39 +0900

    refactor be-dashboard remainingTime 수정, 이미지 추가

--- stargate-be/src/main/java/com/ssafy/stargate/model/service/DashboardServiceImpl.java
+++ stargate-be/src/main/java/com/ssafy/stargate/model/service/DashboardServiceImpl.java
@@ -2,17 +2,24 @@ package com.ssafy.stargate.model.service;
 
 import com.ssafy.stargate.exception.NotFoundException;
 import com.ssafy.stargate.model.dto.common.MeetingDto;
+import com.ssafy.stargate.model.dto.common.SavedFileDto;
+import com.ssafy.stargate.model.dto.response.DashboardMeetingResponseDto;
 import com.ssafy.stargate.model.dto.response.DashboardResponseDto;
 import com.ssafy.stargate.model.entity.Meeting;
 import com.ssafy.stargate.model.entity.MeetingFUserBridge;
 import com.ssafy.stargate.model.repository.MeetingFUserRepository;
 import com.ssafy.stargate.model.repository.MeetingRepository;
+import com.ssafy.stargate.util.FileUtil;
 import jakarta.transaction.Transactional;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
@@ -32,13 +39,19 @@ public class DashboardServiceImpl implements DashboardService{
     @Autowired
     MeetingFUserRepository meetingFUserRepository;
 
-    List<MeetingDto> todayMeetings;
+    List<DashboardMeetingResponseDto> todayMeetings;
 
-    List<MeetingDto> futureMeetings;
+    List<DashboardMeetingResponseDto> futureMeetings;
 
-    List<MeetingDto> pastMeetings;
+    List<DashboardMeetingResponseDto> pastMeetings;
 
     LocalDateTime today;
+
+    @Autowired
+    private FileUtil fileUtil;
+
+    @Value("${s3.filepath.meeting}")
+    private String filePath;
 
     /**
      * 회원 (email) 와 관련된 meeting 정보 조회 
@@ -104,18 +117,48 @@ public class DashboardServiceImpl implements DashboardService{
      * @param meeting Meeting 미팅 정보
      */
     private void classifyMeetings(Meeting meeting){
-
+        
         if(meeting != null){
 
             if(meeting.getStartDate().getDayOfYear() == today.getDayOfYear()){
-                todayMeetings.add(MeetingDto.entityToDto(meeting));
+                todayMeetings.add(setMeetingInfo(meeting));
 
             }else if(meeting.getStartDate().isBefore(today)){
-                pastMeetings.add(MeetingDto.entityToDto(meeting));
+                pastMeetings.add(setMeetingInfo(meeting));
 
             }else if(meeting.getStartDate().isAfter(today)){
-                futureMeetings.add(MeetingDto.entityToDto(meeting));
+                futureMeetings.add(setMeetingInfo(meeting));
             }
+        }
+    }
+
+    /**
+     * 미팅 정보 전달하기 위해 dto 에 정보 저장
+     * @param meeting Meeting 미팅 정보
+     * @return DashboardMeetingResponseDto 대시보드에 있는 미팅 정보 담는 dto
+     */
+    private DashboardMeetingResponseDto setMeetingInfo(Meeting meeting){
+
+        SavedFileDto savedFileDto = fileUtil.getFileInfo(filePath, meeting.getImage());
+
+        long remainingTimeMilli = meeting.getStartDate().atZone(ZoneId.of("Asia/Seoul")).toInstant().toEpochMilli() -LocalDateTime.now().atZone(ZoneId.of("Asia/Seoul")).toInstant().toEpochMilli();
+        long remainingTimeSecond =  TimeUnit.MILLISECONDS.toSeconds(remainingTimeMilli);
+
+        if(remainingTimeSecond >= 0){
+            return DashboardMeetingResponseDto.builder()
+                    .uuid(meeting.getUuid())
+                    .name(meeting.getName())
+                    .startDate(meeting.getStartDate())
+                    .remainingTime(remainingTimeSecond)
+                    .imageFileInfo(savedFileDto)
+                    .build();
+        }else{
+            return DashboardMeetingResponseDto.builder()
+                    .uuid(meeting.getUuid())
+                    .name(meeting.getName())
+                    .startDate(meeting.getStartDate())
+                    .imageFileInfo(savedFileDto)
+                    .build();
         }
     }
 }
