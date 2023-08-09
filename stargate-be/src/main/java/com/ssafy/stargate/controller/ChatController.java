2de13b045a345e39b7d8a2b620f3a7a118c1a7df
dohyeon <beigemint26@gmail.com>
2023-08-09 10:43:32 +0900

    be/feature/stomp-chat http로 연결되는 것 확인

--- stargate-be/src/main/java/com/ssafy/stargate/controller/ChatController.java
+++ stargate-be/src/main/java/com/ssafy/stargate/controller/ChatController.java
@@ -2,16 +2,14 @@ package com.ssafy.stargate.controller;
 
 
 
-
-
 import com.ssafy.stargate.exception.NotFoundException;
 import com.ssafy.stargate.model.dto.common.ChatMessageDto;
 import com.ssafy.stargate.model.dto.request.ChattingRoomRequestDto;
 import com.ssafy.stargate.model.dto.response.ChattingRoomResponseDto;
 import com.ssafy.stargate.model.service.ChatService;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -26,48 +24,50 @@ import java.util.List;
 @Controller
 @Slf4j
 @RequiredArgsConstructor
-//@RequestMapping("/chat")
+@Transactional
+@RequestMapping("/chat")
+@MessageMapping("/chat")
 public class ChatController {
 
     private final ChatService chatService;
 
 
-    @PostMapping("/chat/new-room")
+    @PostMapping("/new-room")
     public ResponseEntity<ChattingRoomResponseDto> createNewRoom(@RequestBody ChattingRoomRequestDto dto) {
         return ResponseEntity.ok(chatService.createChattingRoom(dto));
     }
 
-    @PutMapping("/chat/update-room")
+    @PutMapping("/update-room")
     public ResponseEntity<ChattingRoomResponseDto> updateChatRoom(@RequestBody ChattingRoomRequestDto dto) throws NotFoundException {
         return ResponseEntity.ok(chatService.updateChattingRoom(dto));
     }
 
 
 
-    @GetMapping("/chat/rooms")
+    @GetMapping("/rooms")
     public ResponseEntity<List<ChattingRoomResponseDto>> getAllRooms () {
         return ResponseEntity.ok(chatService.getAllChattingRoom());
     }
 
-    @GetMapping("/chat/messages")
+    @GetMapping("/messages")
     public ResponseEntity<List<ChatMessageDto>> getMessages (@RequestBody ChattingRoomRequestDto dto){
         return ResponseEntity.ok(chatService.getChattingMessages(dto));
     }
 
 
-    @MessageMapping("/chat/{roomNo}")  //app/chat/{roomNo}
+    @MessageMapping("/{roomNo}")  //app/chat/{roomNo}
     @SendTo("/topic/chat/{roomNo}")    // handler 에서 처리 마친 것을 topic/chat 경로로 전송
     public void message (@Payload ChatMessageDto message, @DestinationVariable(value = "roomNo") Long roomNo){
         chatService.sendMessage(message, roomNo);
     }
 
-    @DeleteMapping("/chat/message")
+    @DeleteMapping("/message")
     public ResponseEntity<?> deleteMessage (@RequestBody ChatMessageDto dto){
         chatService.deleteMessage(dto);
         return ResponseEntity.ok(null);
     }
 
-    @PutMapping("/chat/message")
+    @PutMapping("/message")
     public ResponseEntity<?> updateMessage (@RequestBody ChatMessageDto dto){
         return ResponseEntity.ok(chatService.updateMessage(dto));
     }
